873a1ffef6d2e6e50bd011fdb29d5ccb4ccb7a1e
Suetone Carneiro <suetoneneto12@gmail.com>
2024-03-02 20:26:10 -0300

    Fixing up some issues and making tests

--- bank_account.py
+++ bank_account.py
@@ -1,13 +1,13 @@
 class CurrentAccount:
-    def __init__(self, number, balance, name):
+    def __init__(self, name, number, balance):
+        self._name = name
         self._number = number
         self._balance = balance
-        self._name = name
 
     # Gets -----------------------------------------
     @property
     def number(self):
-        return self._name
+        return self._number
         
     @property 
     def balance(self):
@@ -19,17 +19,51 @@ class CurrentAccount:
     # --------------------------------------------------
 
     # Sets
-    @balance.setter
+    @name.setter
+    def change_name(self, new_name):
+        self._name = new_name
+
+    #Methods 
     def withdraw(self, withdraw_value): # return bool: true if there is money enough and false if there isn't
-        if withdraw_value >= self._balance:
+        if self._balance >= withdraw_value:
             self._balance -= withdraw_value
             return True
         else:
             return False
-        
+    
     def make_deposit(self, deposit_value): # Doesn't have 'return', just increase the balance
-        self.balance += deposit_value
+        self._balance += deposit_value
+    
+    def __str__(self) -> str:
+        return f'Name: {self.name}\nNumber: {self.number}\nBalance: ${self.balance}'
 
-    def name(self, new_name):
-        self._name = new_name
+
+# Tests area 
+
+name = str(input('Name of the owner of the bank account: '))
+number = int(input('Account number: '))
+balance = int(input('Initial balance: $ '))
+
+account_demo = CurrentAccount(name, number, balance)
+
+while True:
+    print('\n-------Menu-------')
+    print('[1] Make a deposit\n[2] Withdraw\n[3] Check balance\n[4] Exit')
+    op = int(input('>>> '))
+    if op == 4: 
+        break
+
+    if op == 1:
+        value = int(input('Deposit value: $'))
+        account_demo.make_deposit(value)
+
+    if op == 2:
+        value = int(input('Withdraw value: $'))
+        if not account_demo.withdraw(value):
+            print('Insufficient balance')
+        else:
+            print('Successful withdrawal')
+    
+    if op == 3:
+        print(f'Your balance: ${account_demo.balance}')
     
