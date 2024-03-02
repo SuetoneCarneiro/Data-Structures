9d9edc8aa309638e48fd119a37979fccfbf11f73
Suetone Carneiro <suetoneneto12@gmail.com>
2024-03-02 15:20:02 -0300

    Using @classmethod and creating the __str__ method

--- Videogame.py
+++ Videogame.py
@@ -1,48 +1,32 @@
 from datetime import date
 
-seriesNumber = 10454 # aleatory
-
 class Videogame:
-    def __init__(self, marca=None, modelo=None, fabricacao=None, capacidadeHD=None, numeroDeSerie=None,jogos=None,garantia=None):
+
+    cont_numero_serie = 10324 #aleatory
+
+    def __init__(self, marca=None, modelo=None, data_fabricacao=None, capacidadeHD=None, serie=None,jogos=None,garantia=None):
+        self.incrementa_contador()
         self.marca = marca
         self.modelo = modelo
-        self.fabricacao = fabricacao
+        self.data_fabricacao = data_fabricacao if data_fabricacao else date.today()
         self.capacidade = capacidadeHD
-        self.serie = numeroDeSerie
+        self.serie = self.cont_numero_serie
         self.jogos = jogos
         self.garantia = garantia
 
-        global seriesNumber
+        
+    def __str__(self) -> str:
+        return f'{self.marca}/{self.modelo} - {self.data_fabricacao}'
 
-        if self.fabricacao == None: self.fabricacao = date.today()
+    @classmethod
+    def incrementa_contador(cls):
+        cls.cont_numero_serie += 1
 
-        if self.serie == None:
-            self.serie = seriesNumber
-            seriesNumber += 1
-    
-    def info(self):
-        print(f'Marca: {self.marca}\nModelo: {self.modelo}\nData de fabricação: {self.fabricacao}')
-        print(f'Capacidade do HD: {self.capacidade}\nNº de série: {self.serie}\nJogos: {self.jogos}')
-        print(f'Garantia: {self.garantia}')
 
-def linha():
-    print('-'*25)
-
-vg1 = Videogame()
-vg2 = Videogame(fabricacao='2024-02-25')
-vg3 = Videogame(fabricacao='204-02-24', marca='Microsoft', modelo='Xbox One')
-vg4 = Videogame(marca='Sony', modelo='PlayStation 2', fabricacao='2024-01-02',
+vg1 = Videogame(data_fabricacao='2024-02-24', marca='Microsoft', modelo='Xbox One')
+vg2 = Videogame(marca='Sony', modelo='PlayStation 2', data_fabricacao='2024-01-02',
                 capacidadeHD='100GB', jogos='GTA SAN ANDREAS, BOMBA PATCH FOREVER, MIDNIGHT CLUB 3, GUITAR HERO BRAZUCAS', garantia='Eterna')
 
-linha()
-print('Video game 1')
-vg1.info()
-linha()
-print('Video game 2')
-vg2.info()
-linha()
-print('Video game 3')
-vg3.info()
-linha()
-print('Video game 4')
-vg4.info()
+
+print('Video game 1: ', vg1)
+print('Video game 2: ', vg2)
