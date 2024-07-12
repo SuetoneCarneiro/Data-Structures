9c11962a00a6d27f72ab3202762b3f4d2700c273
Suetone Carneiro <suetoneneto12@gmail.com>
2024-07-12 14:10:14 -0300

    studying

--- data-structures/array_ifpb.py
+++ data-structures/array_ifpb.py
@@ -1,10 +1,17 @@
 class Array:
 
-    def __init__(self, length):
+    def __init__(self, length: int):
         self.lenght = length
         self._data = [None]*length
 
+
+    def position_check(self, position):
+        if not position >= 0:
+            raise IndexError('Invalid position')
+        
     def set(self, position: int, value):
+        if not position >= 0:
+            raise IndexError('Invalid position')
         self._data[position] = value
     
     def __setitem__(self, position: int, value):
@@ -16,5 +23,7 @@ class Array:
     def __getitem__(self, position: int):
         return self.get(position)
     
-    
+    def copy_from(self, another_array):
+        for i in range(another_array.len):
+            self.set(i, another_array.get(i))
     
--- data-structures/stack.py
+++ data-structures/stack.py
@@ -4,10 +4,10 @@ class Stack:
     def __init__(self):
         ...
     
-    def stack_up(self, element):
+    def push(self, element):
         ...
     
-    def unstack(self):
+    def pop(self):
         ...
     
     def show(self):
@@ -21,10 +21,10 @@ class EasyStack:
     def __init__(self):
         self._data = []
     
-    def stack_up(self, element):
+    def push(self, element):
         self._data.append(element)
 
-    def unstack(self):
+    def pop(self):
         return self._data.pop()
     
     def show(self):
@@ -37,33 +37,77 @@ class EasyStack:
         return self._data[-1]
     
 
+class EmptyStack(Exception):
+    pass
+
+
+class FullStack(Exception):
+    pass
+
+
 class SequencialStack(Stack):
     def __init__(self):
-        self._data = Array(length=10)
-        self._top = 9
-    
-    def stack_up(self, element):
-        self._data[self._top - 1] = self._data[self._top]
-        self._data.set(self._top, element)
-    
-    #FIX ME:
-    def unstack(self):
-        self._data.set(self._top, None)
-    
-    def show(self):
-        for i in range(10):
-            print(self._data.get(i), end=' ')
-    
+        self._data = Array(length=3)
+        self._top = 0
+
     @property
     def top(self):
-        return self._data.get(self._top)
+        if self.is_empty:
+            raise EmptyStack()
+        return self._data.get(self._top - 1)
+    
+    @property
+    def len(self):
+        return self._top
+
+    @property
+    def is_empty(self):
+        return self.len == 0
+    
+    @property
+    def is_full(self):
+        return self.len == self._data.lenght
+    
+    def _resize(self):
+        old_data = self._data
+        new_data = Array(old_data.lenght * 2)
+        self._data = new_data
+        self._data.copy_from(old_data)
+
+
+    def push(self, element):
+        if self.is_full:
+            self._resize()
+
+        self._data.set(self._top, element)
+        self._top += 1
+    
+
+    def pop(self):
+        if self.is_empty:
+            raise EmptyStack()
+        
+        top = self.top
+        self._top -= 1
+        self._data.set(self._top, None)
+        return top
+    
+    def show(self):
+        for i in range(self._top):
+            print(self._data.get(i), end=' ')
+        print()
+    
 
 def sequencial_stack_test():
     stack = SequencialStack()
-    stack.stack_up(1)
-    stack.stack_up(2)
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
     stack.show()
-    assert stack.top == 2
+    assert stack.pop() == 3
+    assert stack.top == 3
+    stack.show()
+    assert stack.is_full == False
 
 
 if __name__ == '__main__':
