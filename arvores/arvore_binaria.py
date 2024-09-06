d724d2eaa59115d8835f365b2c652b8118b0a532
Suetone Carneiro <suetoneneto12@gmail.com>
2024-09-06 10:21:25 -0300

    metodos: count, altura, leafs, get_level, liberar e remocao

--- arvores/arvore_binaria.py
+++ arvores/arvore_binaria.py
@@ -91,3 +91,94 @@ class Arvore:
         else:
             return self._busca(no.filho_esquerda, valor)
 
+    def count(self):
+        self._count(self.raiz)
+
+    def _count(self, no: Node | None):
+        if no is None:
+            return 0
+        return 1 + self._count(no.filho_direita) + self._count(no.filho_esquerda)
+    
+    @property
+    def altura(self) -> int:
+        return self._altura(self.raiz)
+
+    def _altura(self, no: Node | None) -> int:
+        if no is None:
+            return 0
+        
+        alt_esquerda = self._altura(no.filho_esquerda)
+        alt_direita = self._altura(no.filho_direita)
+        
+        return max(alt_esquerda, alt_direita)
+    
+    @property
+    def leafs(self) -> int:
+        return self._leafs(self.raiz)
+
+    def _leafs(self, no: Node | None) -> int:
+        if no is None:
+            return 0
+        
+        if no.eh_folha:
+            return 1
+        
+        return self._leafs(no.filho_esquerda) + self._leafs(no.filho_direita)
+    
+    def get_level(self, key):
+        return self._get_level(self.raiz, key, nivel=0)
+    
+    def _get_level(self, no: Node | None, key, nivel) -> int:
+        if no is None:
+            raise NodeNotFound()
+        elif no.valor == key:
+            return nivel
+        elif key > no.valor:
+            return self._get_level(no.filho_direita, key, nivel+1)
+        else:
+            return self._get_level(no.filho_esquerda, key, nivel+1)
+    
+    def liberar(self, valor) -> bool:
+        return self._liberar(self.raiz, valor)
+
+    def _liberar(self, no: Node | None, valor) -> bool:
+        if no is None:
+            return False
+        elif no.valor == valor:
+            no.filho_esquerda = None
+            no.filho_direita = None
+            return True
+        elif no > no.valor:
+            return self._liberar(no.filho_direita, valor)
+        else:
+            return self._liberar(no.filho_esquerda, valor)
+        
+    def _get_sucessor(self, no: Node) -> Node:
+        atual: Node = no
+        while atual.tem_filho_esquerda:
+            atual = atual.filho_esquerda
+        return atual
+    
+    def remocao(self, valor):
+        return self._remocao(self.raiz, valor)
+
+    def _remocao(self, no: Node| None, valor) -> Node | None:
+        if no is None:
+            return None
+        
+        if valor < no.valor:
+            no.filho_esquerda = self._remocao(no.filho_esquerda,  valor)
+        elif valor > no.valor:
+            no.filho_direita = self._remocao(no.filho_direita, valor)
+        else:
+            if not no.tem_filho_direita:
+                return no.filho_esquerda
+            elif not no.tem_filho_esquerda:
+                return no.filho_direita
+            else:
+                substituto = self._get_sucessor(no.filho_direita)
+                no.valor = substituto.valor
+                no.filho_direita = self._remocao(no.filho_direita, no.valor)
+        
+        return no
+
